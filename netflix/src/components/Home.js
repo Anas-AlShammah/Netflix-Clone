006c5da1b6e7bccd49abcdd54f18f3873aac0c3d
anasgharaibeh <anas.gharaibeh1@yahoo.com>
2023-05-17 10:19:58 +0000

    18

--- netflix/src/components/Home.js
+++ netflix/src/components/Home.js
@@ -1,7 +1,28 @@
+import axios from 'axios';
+import { useEffect,useState } from 'react';
+import MovieList from './MovieList';
 export default function Home() {
+	const [movie,setMovie]=useState([]);
+	const getallfav = () =>{
+		const url='http://localhost:3006/trend'
+		axios.get(url)
+        .then(response=>{
+					setMovie(response.data)
+            console.log(response.data)
+        })
+        .catch((error)=>{
+            console.log(error)
+        })
+	}
+	console.log(movie)
+	useEffect(()=>{
+		getallfav();
+	},[])
 	return (
 		<>
-
+		<h1>HomePage</h1>
+		<MovieList movie={movie}/>
+		
 		</>
 	)
 }
--- netflix/src/components/MovieList.js
+++ netflix/src/components/MovieList.js
@@ -1,7 +1,13 @@
-export default function MovieList() {
+import Movie from "./Movie"
+export default function MovieList({movie}) {
 	return (
 		<>
-
+		
+		{movie.map(e=>(
+			<ul key={e.id}>
+<Movie e={e}/>
+			</ul>
+		))}
 		</>
 	)
 }
